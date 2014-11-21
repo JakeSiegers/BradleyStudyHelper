15d29c09a29a595640d9940fc92ff0492ad0c5af
Griffin Polonus <gpolonus@mail.bradley.edu>
2014-11-20 18:55:38 -0600

    Made the UI and added stuff

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+COOL STUFF
+</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<style>
+*
+{
+	font-family: Comic Sans;
+}
+</style>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Bradley Study Helper</h1>
+			<form>
+				<div class="form-group">
+					<select name="dept" class="form-control">
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<select name="class" class="form-control">
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<select name="section" class="form-control">
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+						<option>Test</option>
+					</select>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6" style="background:red;height:200px;">
+			Text
+		</div>
+		<div class="col-md-6" style="background:green;height:300px;">
+			Burgers
+		</div>
+	</div>
+</div>
+</body>
+</html>
